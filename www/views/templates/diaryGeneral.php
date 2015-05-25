28660983dff9ee678bf6d486200389d06e57b6a7
aleksey <kovalchuk.aleksey.s@gmail.com>
2015-05-25 23:34:07 +0300

    Frontend Diary Layout

--- www/views/templates/diaryGeneral.php
+++ www/views/templates/diaryGeneral.php
@@ -6,11 +6,48 @@
  * Time: 15:50
  */
 ?>
-<div class="row">
-    <div class="col-md-4 col-sm-4">
-        <label>Дата:</label>
-    </div>
-    <div class="col-md-8 col-sm-8">
 
+<div class="row">
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        <div class="row form-group">
+            <div class="col-md-5 col-sm-5 col-sx-12">
+                <label><?=Yii::t('ui', 'Height');?>:</label>
+            </div>
+            <div class="col-md-7 col-sm-7 col-sx-12">
+                <input type="text" class="form-control" value="test" />
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        <div class="row form-group">
+            <div class="col-md-5 col-sm-5 col-sx-12">
+                <label><?=Yii::t('ui', 'Head circumference');?>:</label>
+            </div>
+            <div class="col-md-7 col-sm-7 col-sx-12">
+                <input type="number" class="form-control" value="64" />
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        <div class="row form-group">
+            <div class="col-md-5 col-sm-5 col-sx-12">
+                <label><?=Yii::t('ui', 'Weight');?>:</label>
+            </div>
+            <div class="col-md-7 col-sm-7 col-sx-12">
+                <input type="number" class="form-control" value="test" />
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        <div class="row form-group">
+            <div class="col-md-5 col-sm-5 col-sx-12">
+                <label><?=Yii::t('ui', 'Chest circumference');?>:</label>
+            </div>
+            <div class="col-md-7 col-sm-7 col-sx-12">
+                <input type="text" class="form-control" value="test" />
+            </div>
+        </div>
     </div>
 </div>
