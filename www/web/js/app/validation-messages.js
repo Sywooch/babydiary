511f2d45501fddc672d0a3e33a011c4d2559b748
freedom <nazarenko.ts@gmail.com>
2015-08-13 22:55:32 +0300

    Session storage for validation messages(isn't used)

--- www/web/js/app/validation-messages.js
+++ www/web/js/app/validation-messages.js
@@ -3,13 +3,33 @@
  */
 var ValidationMessages = {
     messages : {},
+    saveToSessionStorage: function() {
+        var storage = window['sessionStorage'];
+        if (!_.isUndefined(storage))storage.setItem('validation-messages', JSON.stringify(this.messages));
+    },
+    loadFromSessionStorage: function() {
+        var storage = window['sessionStorage'];
+        //storage.removeItem('validation-messages');
+        if (!_.isUndefined(storage)) {
+            var retrievedObject = storage.getItem('validation-messages');
+            if (retrievedObject) {
+                this.messages = JSON.parse(retrievedObject);
+                this.updateBackboneValidationMessages();
+                return true;
+            }
+        }
+        return false;
+    },
     loadMessages: function() {
-        var self = this;
-        AjaxHelper.getSync('api/get-validation-messages', null,
-            function(data) {
-                self.messages = data;
-                self.updateBackboneValidationMessages();
-            });
+        //if (!this.loadFromSessionStorage()) {
+            var self = this;
+            AjaxHelper.getSync('api/get-validation-messages', null,
+                function (data) {
+                    self.messages = data;
+                    self.updateBackboneValidationMessages();
+                    //self.saveToSessionStorage();
+                });
+        //}
     },
     updateBackboneValidationMessages: function() {
         for (var prop in Backbone.Validation.messages) {
@@ -17,14 +37,7 @@ var ValidationMessages = {
                 Backbone.Validation.messages[prop] = this.messages[prop];
             }
         }
-        //_.extend(Backbone.Validation.messages, {
-        //    required: ValidationMessages.get('required'),
-        //    minLength: ValidationMessages.get('minLength'),
-        //    maxLength: ValidationMessages.get('maxLength'),
-        //    email: ValidationMessages.get('emailPattern'),
-        //    login: ValidationMessages.get('loginPattern'),
-        //    password: ValidationMessages.get('passwordPattern')
-        //});
+
     },
     get: function(key) {
         return this.messages[key];
