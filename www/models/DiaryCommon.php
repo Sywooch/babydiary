00d52bc2582eecb12f93524302d43d94a29dd210
Aleksey Kovalchuk <kovalchuk.aleksey.s@gmail.com>
2015-08-18 23:48:03 +0300

    Form validation icon

--- www/models/DiaryCommon.php
+++ www/models/DiaryCommon.php
@@ -9,14 +9,12 @@ use Yii;
  *
  * @property integer $diary_common_id
  * @property integer $diary_id
- * @property string $photo
  * @property string $weight
  * @property integer $height
  * @property integer $head_circumference
  * @property integer $chest_circumference
  * @property string $other
  * @property string $notes
- * @property string $spelling
  *
  * @property Diary $diary
  */
@@ -36,11 +34,11 @@ class DiaryCommon extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['diary_common_id', 'diary_id', 'photo', 'weight', 'height', 'head_circumference', 'chest_circumference', 'other', 'notes', 'spelling'], 'required'],
+            [['diary_common_id', 'diary_id', 'weight', 'height', 'head_circumference', 'chest_circumference', 'other', 'notes'], 'required'],
             [['diary_common_id', 'diary_id', 'height', 'head_circumference', 'chest_circumference'], 'integer'],
             [['weight'], 'number'],
-            [['notes', 'spelling'], 'string'],
-            [['photo', 'other'], 'string', 'max' => 255]
+            [['notes'], 'string'],
+            [['other'], 'string', 'max' => 255]
         ];
     }
 
@@ -52,14 +50,12 @@ class DiaryCommon extends \yii\db\ActiveRecord
         return [
             'diary_common_id' => Yii::t('ui', 'Diary Common ID'),
             'diary_id' => Yii::t('ui', 'Diary ID'),
-            'photo' => Yii::t('ui', 'Photo'),
             'weight' => Yii::t('ui', 'Weight'),
             'height' => Yii::t('ui', 'Height'),
             'head_circumference' => Yii::t('ui', 'Head Circumference'),
             'chest_circumference' => Yii::t('ui', 'Chest Circumference'),
             'other' => Yii::t('ui', 'Other'),
             'notes' => Yii::t('ui', 'Notes'),
-            'spelling' => Yii::t('ui', 'Spelling'),
         ];
     }
 
